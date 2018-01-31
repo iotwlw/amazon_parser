27d5e47f2bd84186bf01bb60d1285ff42a738a04
dg1245 <qitiandalong@gmail.com>
2018-01-31 18:08:45 +0800

    Update amazon_completion.py
    增加中文注释

--- amazon_completion.py
+++ amazon_completion.py
@@ -1,5 +1,8 @@
+# 在amazon.com搜索框搜索词的时候，抓取下拉菜单提示词
+
 from amazon_module import amazon_module
 
+# 填入要搜索的词，注意：词结尾有无空格，结果会不同
 keyword_list = [
     "cat toy",
     "dog collar",
@@ -8,6 +11,7 @@ keyword_list = [
 def keyword_to_long_tail_keyword_list(keyword):
     try:
         print("keyword:", keyword)
+        # ------这样的横线对应搜索词中字符的数量，如果-有多，可能是搜索词前后有空格
         print("-" * (len("keyword: ") + len(keyword)))
         url_head = "https://completion.amazon.com/search/complete?method=completion&mkt=1&r=Y5KKREBZPVVDRZT19HX9&s=133-8959284-8300960&c=&p=Gateway&l=en_US&b2b=0&fresh=0&sv=desktop&client=amazon-search-ui&x=String&search-alias=aps&q="
         url_tail = "&qs=&cf=1&fb=1&sc=1&"
@@ -39,10 +43,10 @@ def keyword_to_long_tail_keyword_list(keyword):
 print("根据给定的词，获取亚马逊搜索框提示词；")
 print("")
 
-abc_list = [chr(i) for i in range(97,123)]
-abc_with_blank_list = [chr(i) for i in range(97,123)]
-abc_with_blank_list.insert(0, "")
-abc_with_blank_list.insert(1, " ")
+# abc_list = [chr(i) for i in range(97,123)]
+# abc_with_blank_list = [chr(i) for i in range(97,123)]
+# abc_with_blank_list.insert(0, "")
+# abc_with_blank_list.insert(1, " ")
 # print(abc_list)
 # print(abc_with_blank_list)
 
