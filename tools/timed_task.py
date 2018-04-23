be3181b2a40c90b3884842592b673815a75ff496
QuanQuan <iotwlw@126.com>
2018-04-23 14:35:09 +0800

    change cron

--- tools/timed_task.py
+++ tools/timed_task.py
@@ -10,7 +10,7 @@ logging.basicConfig()
 schedular = BackgroundScheduler()
 
 
-@schedular.scheduled_job('cron', hour='0-12', minute=11, second=11)
+@schedular.scheduled_job('cron', hour='0,2,4,6,8,10', minute=03, second=11)
 def timed_job():
     print ("{}------------SHUIKU Search Engines--------RUN----------------".format(datetime.datetime.now()))
     product_detail_mysql.product_detail_to_mysql()
