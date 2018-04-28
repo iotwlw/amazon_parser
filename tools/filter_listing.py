dcb0aa8d0b729412b59531e95f1b2c978a4e6105
QuanQuan <iotwlw@126.com>
2018-04-28 12:38:01 +0800

    update list_google Data table

--- tools/filter_listing.py
+++ tools/filter_listing.py
@@ -1,7 +1,9 @@
 # ! /usr/bin/env python
 # -*- coding:utf-8 -*-
 # __author__ = "TKQ"
+import os
 import random
+import time
 
 import pymysql
 import contextlib
@@ -22,22 +24,27 @@ def mysql(host='127.0.0.1', port=3306, user='root', passwd='P@ssw0rd', db='amazo
         cursor.close()
         conn.close()
 
-# 执行sql
-with mysql() as cursor:
-    print(cursor)
-    row_count = cursor.execute("select * from listing_google where state = 0 limit  1")
-    print "----------------------------------------------------------------ALL ASIN:"+str(row_count)
-    for row in cursor.fetchall():
-        try:
-            asin = row["asin"]
-            print ("{}:------------".format(datetime.datetime.now()) + asin + "----------------begin")
-            listing_info_dict, ranking_list, offering_list = merge.asin_to_listing_info(asin)
-            merge.insert_data_to_mysql(listing_info_dict, "merge_product_detail")
-            merge.insert_mysql(ranking_list, "merge_product_salesrank")
-            merge.insert_mysql(offering_list, "merge_product_offer")
-            sleep_time = random.randint(7, 100)
-            print ("{}:----------------Sleep:{}".format(datetime.datetime.now(), sleep_time) + "------end")
-            time.sleep(sleep_time)
-        except Exception as e:
-            print("Product_detail Error {}".format(e))
 
+# 执行sql
+def filter_listing():
+    with mysql() as cursor:
+        try:
+            row_count = cursor.execute("select DISTINCT asin from listing_google where state = 0")
+            print "---------------------------ALL ASIN:"+str(row_count)+"---------------------------"
+            data_asin = []
+            for row in cursor.fetchall():
+                    asin = row["asin"]
+                    print ("{}:------------".format(datetime.datetime.now()) + asin + "----------------begin")
+                    listing_info_dict, ranking_list, offering_list, review_dict_list = merge.asin_to_listing_info(asin)
+                    merge.insert_data_to_mysql(listing_info_dict, "merge_product_detail")
+                    merge.insert_mysql(ranking_list, "merge_product_salesrank")
+                    merge.insert_mysql(offering_list, "merge_product_offer")
+                    merge.insert_mysql(review_dict_list, "merge_product_review")
+                    data_asin.append(asin)
+            merge.update_listing_google(data_asin)
+        except Exception as e:
+            # os.system('start G:\911S5\ProxyTool\AutoProxyTool.exe  -changeproxy/US')
+            merge.update_listing_google(data_asin)
+            print("{}".format(e))
+
+filter_listing()
