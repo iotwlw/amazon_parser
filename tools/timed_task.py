9f59285a03d7a045ed667a9faec4423ff086dfc3
QuanQuan <iotwlw@126.com>
2018-04-28 11:39:32 +0800

    uk monitor

--- tools/timed_task.py
+++ tools/timed_task.py
@@ -10,10 +10,16 @@ logging.basicConfig()
 schedular = BackgroundScheduler()
 
 
-@schedular.scheduled_job('cron', hour='0,2,4,6,8,10', minute=03, second=11)
+@schedular.scheduled_job('cron', hour='0,2,4,6,8,9', minute=32, second=45)
 def timed_job():
     print ("{}------------SHUIKU Search Engines--------RUN----------------".format(datetime.datetime.now()))
     product_detail_mysql.product_detail_to_mysql()
+
+
+@schedular.scheduled_job('cron', hour='0,2,4,6,8,9', minute=40, second=45)
+def timed_job():
+    print ("{}------------SHUIKU UK Search Engines--------RUN----------------".format(datetime.datetime.now()))
+    product_detail_mysql.product_detail_to_mysql("UK")
 
 schedular.start()
 os.system('pause')
